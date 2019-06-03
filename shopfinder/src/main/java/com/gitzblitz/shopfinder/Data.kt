62a84623e0d7141fedc5682c2ffe842d041e6c4d
George Ng'ethe <gitzblitz@users.noreply.github.com>
2019-06-03 21:51:36 +0200

    Added Data models and AppExecutors

---
+++ shopfinder/src/main/java/com/gitzblitz/shopfinder/Data.kt
@@ -0,0 +1,33 @@
+package com.gitzblitz.shopfinder
+import com.squareup.moshi.Json
+
+
+data class Data(
+    @Json(name = "cities")
+    val cities: List<City>
+)
+
+data class City(
+    @Json(name = "id")
+    val id: Int,
+    @Json(name = "malls")
+    val malls: List<Mall>,
+    @Json(name = "name")
+    val name: String
+)
+
+data class Mall(
+    @Json(name = "id")
+    val id: Int,
+    @Json(name = "name")
+    val name: String,
+    @Json(name = "shops")
+    val shops: List<Shop>
+)
+
+data class Shop(
+    @Json(name = "id")
+    val id: Int,
+    @Json(name = "name")
+    val name: String
+)
