fd80c0e8fc8f9e9ef5f2039b9e275843cd257893
George Ng'ethe <gitzblitz@users.noreply.github.com>
2019-06-05 23:40:56 +0200

    Returning singles in Webservice

--- shopfinder/src/main/java/com/gitzblitz/shopfinder/Webservice.kt
+++ shopfinder/src/main/java/com/gitzblitz/shopfinder/Webservice.kt
@@ -1,29 +1,33 @@
 package com.gitzblitz.shopfinder
 
+import com.gitzblitz.shopfinder.model.City
+import com.gitzblitz.shopfinder.model.Mall
+import com.gitzblitz.shopfinder.model.Shop
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface Webservice {
 
     @GET("cities")
-    fun getAllCities()
+    fun getAllCities(): Single<List<City>?>
 
     @GET("city/{cityId}/malls")
-    fun getMallsByCity(@Path("cityId") cityId: String)
+    fun getMallsByCity(@Path("cityId") cityId: String): Single<List<Mall>?>
 
     @GET("city/{cityId}/mall/{mallId}")
     fun getMall(
         @Path("cityId") cityId: String,
         @Path("mallId") mallId: String
-    )
+    ): Single<Mall>
 
     @GET("city/{cityId}")
-    fun getCity(@Path("cityId") cityId: String)
+    fun getCity(@Path("cityId") cityId: String): Single<City>
 
     @GET("city/{cityId}/mall/{mallId}/shop/{shopId}")
     fun getShop(
         @Path("cityId") cityId: String,
         @Path("mallId") mallId: String,
         @Path("shopId") shopId: String
-    )
+    ): Single<Shop>
 }
--- shopfinder/src/main/java/com/gitzblitz/shopfinder/model/Resource.kt
+++ shopfinder/src/main/java/com/gitzblitz/shopfinder/model/Resource.kt
@@ -1,2 +1,7 @@
 package com.gitzblitz.shopfinder.model
 
+sealed class Resource<out T> {
+    class Loading<out T> : Resource<T>()
+    data class Success<out T>(val data: T?) : Resource<T>()
+    data class Failure<out T>(val throwable: Throwable) : Resource<T>()
+}
