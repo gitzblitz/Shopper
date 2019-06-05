0ce1a4cc7f4efe154c718e5aaca92db047bec713
George Ng'ethe <gitzblitz@users.noreply.github.com>
2019-06-05 23:40:25 +0200

    Added ServiceImpl and WebServiceFacade

---
+++ shopfinder/src/main/java/com/gitzblitz/shopfinder/ServiceImpl.kt
@@ -0,0 +1,88 @@
+package com.gitzblitz.shopfinder
+
+import com.gitzblitz.shopfinder.model.City
+import com.gitzblitz.shopfinder.model.Mall
+import com.gitzblitz.shopfinder.model.Shop
+import io.reactivex.schedulers.Schedulers
+
+object ServiceImpl {
+
+    private val service = ServiceGenerator.provideWebService()
+
+
+    fun getCities(): List<City> {
+        var cities2 = listOf<City>()
+
+        service.getAllCities()
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe({ cities ->
+                cities.let {
+                    cities2 = it ?: listOf()
+                }
+            }, {
+
+            })
+
+        return cities2
+
+    }
+
+    fun getMallsByCity(cityId: String): List<Mall> {
+        var malls = listOf<Mall>()
+
+        service.getMallsByCity(cityId)
+            .subscribeOn(Schedulers.io())
+            .subscribe { result, error ->
+                result.let {
+                    malls = it ?: listOf()
+                }
+
+                error.printStackTrace()
+            }
+
+        return malls
+    }
+
+    fun getMall(cityId: String, mallId: String): Mall {
+        var mall = Mall()
+        service.getMall(cityId, mallId)
+            .subscribeOn(Schedulers.io())
+            .subscribe { result, error ->
+                mall = result
+                error.printStackTrace()
+            }
+
+        return mall
+    }
+
+    fun getCity(cityId: String): City {
+        var city = City()
+        service.getCity(cityId)
+            .subscribeOn(Schedulers.io())
+            .subscribe { result, error ->
+                city = result
+
+                error.printStackTrace()
+            }
+
+        return city
+    }
+
+    fun getShop(cityId: String, mallId: String, shopId: String): Shop {
+        var shop = Shop()
+
+        service.getShop(cityId, mallId, shopId)
+            .subscribeOn(Schedulers.io())
+            .subscribe { result, error ->
+                shop = result
+
+                error.printStackTrace()
+            }
+
+        return shop
+
+    }
+
+
+}
---
+++ shopfinder/src/main/java/com/gitzblitz/shopfinder/interfaces/WebServiceFacade.kt
@@ -0,0 +1,22 @@
+package com.gitzblitz.shopfinder.interfaces
+
+import com.gitzblitz.shopfinder.ServiceImpl
+import com.gitzblitz.shopfinder.model.City
+import com.gitzblitz.shopfinder.model.Mall
+import com.gitzblitz.shopfinder.model.Shop
+
+// this will be a facade singleton class, entry point into the and on
+class WebServiceFacade {
+
+    fun getMallsByCity(cityId: String): List<Mall> = ServiceImpl.getMallsByCity(cityId)
+
+    fun getCities(): List<City> = ServiceImpl.getCities()
+
+    fun getMall(cityId: String, mallId: String): Mall = ServiceImpl.getMall(cityId, mallId)
+
+    fun getCity(cityId: String): City = ServiceImpl.getCity(cityId)
+
+    fun getShop(cityId: String, mallId: String, shopId: String): Shop = ServiceImpl.getShop(cityId,mallId,shopId)
+
+
+}
